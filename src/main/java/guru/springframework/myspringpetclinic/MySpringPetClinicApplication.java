a965236c8a33f39e98277e9db707373f32e152c2
Lele97 <gabriele.grandinetti@hotmail.com>
2023-07-19 10:49:01 +0200

    Initial commit
    add project to repository

---
+++ src/main/java/guru/springframework/myspringpetclinic/MySpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.myspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/myspringpetclinic/MySpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.myspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
